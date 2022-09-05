1aea0935d6ac1bfee4df7dedb142a8abad55bf48
HandsomeFishYin <346107057@qq.com>
2022-09-05 16:46:13 +0800

    wip: 视觉

--- pages/components/index.js
+++ pages/components/index.js
@@ -158,6 +158,20 @@ Page({
       list,
     })
   },
+  kindToggle2: function (e) {
+    const id = e.currentTarget.dataset.id
+    const list = this.data.list2
+    for (let i = 0, len = list.length; i < len; ++i) {
+      if (list[i].id === id) {
+        list[i].open = !list[i].open
+      } else {
+        list[i].open = false
+      }
+    }
+    this.setData({
+      list2: list,
+    })
+  },
   onPullDownRefresh() {
     console.log('demo onPullDownRefresh 触发执行')
   },
