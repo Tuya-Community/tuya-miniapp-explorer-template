578ae774fe854f3009d8cb8383f6814bbedfadb6
HandsomeFishYin <346107057@qq.com>
2022-11-25 15:59:44 +0800

    wip: api i18n

--- pages/pageAPI/getSystemInfo/index.js
+++ pages/pageAPI/getSystemInfo/index.js
@@ -1,27 +1,27 @@
 Page({
-  data: {
-    systemInfo: {
-      brand: '未获取',
-      model: '未获取',
-      language: '未获取',
-      windowWidth: '未获取',
-      windowHeight: '未获取',
-      pixelRatio: '未获取',
+    data: {
+        systemInfo: {
+            brand: i18n.t('getSystemInfo_8'),
+            model: i18n.t('getSystemInfo_8'),
+            language: i18n.t('getSystemInfo_8'),
+            windowWidth: i18n.t('getSystemInfo_8'),
+            windowHeight: i18n.t('getSystemInfo_8'),
+            pixelRatio: i18n.t('getSystemInfo_8'),
+        },
     },
-  },
-  getSystemInfo() {
-    let self = this
-    ty.getSystemInfo({
-      success(res) {
-        console.log(`getSystemInfo ==== success`, res)
-        self.setData({
-          systemInfo: res,
+    getSystemInfo() {
+        let self = this
+        ty.getSystemInfo({
+            success(res) {
+                console.log(`getSystemInfo ==== success`, res)
+                self.setData({
+                    systemInfo: res,
+                })
+            },
         })
-      },
-    })
-  },
-  getSystemInfoSync() {
-    const info = ty.getLaunchOptionsSync()
-    console.log('getLaunchOptionsSync ==== success', info)
-  },
+    },
+    getSystemInfoSync() {
+        const info = ty.getLaunchOptionsSync()
+        console.log('getLaunchOptionsSync ==== success', info)
+    },
 })
